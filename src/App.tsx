f58142ff0fd721162d84f3b6b75a6dd4f3eb8c7d
Cameron Tacklind <cameron@tacklind.com>
2025-05-19 22:49:56 -0700

    Basic usage

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useBackend } from "./backend";
 
 function App() {
   const [count, setCount] = useState(0);
+  const test = useBackend(s => s.reservations.length + count, [count]);
 
   return (
     <>
@@ -18,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount(count => count + 1)}>count is {test}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>

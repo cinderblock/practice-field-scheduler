d555ea6367551827c6fc56b0c040c376ef7703dd
Cameron Tacklind <cameron@tacklind.com>
2025-05-22 02:31:29 -0700

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,12 @@ import Link from "next/link";
 import { LatestPost } from "~/app/_components/post";
 import { ReservationCalendar } from "~/app/_components/ReservationCalendar";
 import { auth } from "~/server/auth";
-import { HydrateClient, api } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import styles from "./index.module.css";
 import type { Session } from "next-auth";
 
 export default async function Home() {
   const session = await auth();
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
 
   return (
     <HydrateClient>

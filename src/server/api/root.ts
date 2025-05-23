d646b0ca5ea028f3ed3144773cb707c0c47be09b
Cameron Tacklind <cameron@tacklind.com>
2025-05-23 00:45:49 -0700

    Remove vestigial API example

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 console.log("Loading api/root.ts");
 
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { reservationRouter } from "./routers/reservation";
 
@@ -10,7 +9,6 @@ import { reservationRouter } from "./routers/reservation";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-	post: postRouter,
 	reservation: reservationRouter,
 });
 

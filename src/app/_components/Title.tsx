f7e99b0e9073cc3b05beb60cb9c4b5909c90faee
Cameron Tacklind <cameron@tacklind.com>
2025-06-04 01:54:25 -0700

    🏳️‍🌈

--- src/app/_components/Title.tsx
+++ src/app/_components/Title.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import styles from "../index.module.css";
+import { useInterval } from "./useInterval";
+
+function isPride() {
+	const now = new Date();
+	return now.getMonth() === 5; // June is month 5 (0-based)
+}
 
 export function Title() {
+	const showRainbow = useInterval(isPride, 1000);
+
 	const classes = [styles.title];
+
+	if (showRainbow) classes.push(styles.rainbowText, styles.rainbowTextAnimated);
 
 	return <h1 className={classes.join(" ")}>Practice Field Scheduler</h1>;
 }

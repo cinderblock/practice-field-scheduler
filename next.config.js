c1823b6ed3caaabcfa36c41935264895d53bba0a
Cameron Tacklind <cameron@tacklind.com>
2025-06-07 20:51:35 -0700

    Enable serving pictures from slack/gravatar

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
 	allowedDevOrigins: [process.env.NEXTAUTH_URL?.replace("https://", "") ?? "localhost"],
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.slack-edge.com",
+			},
+			{
+				protocol: "https",
+				hostname: "secure.gravatar.com",
+			},
+		],
+	},
 };
 
 export default config;
